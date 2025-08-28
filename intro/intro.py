af90646f773412ba478a6b96b61b4b78fac9741d
ibrahim-abdirashid <neeykaraamo@gmail.com>
2025-08-29 00:01:42 +0300

    still variable

--- intro/intro.py
+++ intro/intro.py
@@ -25,3 +25,11 @@ x = y =z = "orange"
 print(x)
 print(y)
 print(z)
+
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+
+fruits = ["tufaax", "liin", "seytuun"]
+x , y,z = fruits
+print(x)
+print(y)
+print(z)
