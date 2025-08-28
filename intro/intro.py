80b5f761360f9dbe5b40ea416ccd2b26338350f0
ibrahim-abdirashid <neeykaraamo@gmail.com>
2025-08-29 00:04:13 +0300

    variables

--- intro/intro.py
+++ intro/intro.py
@@ -33,3 +33,9 @@ x , y,z = fruits
 print(x)
 print(y)
 print(z)
+
+#In the print() function, you output multiple variables, separated by a comma:
+x = "is"
+y = "python"
+z = "awesome"
+print(x, y ,z)
