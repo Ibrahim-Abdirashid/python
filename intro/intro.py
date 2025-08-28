a92e75342fa613bb837b774b4b6894f4e4ea3403
ibrahim-abdirashid <neeykaraamo@gmail.com>
2025-08-28 23:54:06 +0300

    varibales

--- intro/intro.py
+++ intro/intro.py
@@ -1,11 +1,22 @@
 print("hello world")
 # variables
+
+"""
 a =5
 b = 10
 print(type(a))
 print(b)
+"""
+
+
 
 name = "Ibrahim Abdirashid Ibrahim"
 print(name)
+
+#many value to multiple variables
+x, y , z = "orange", "banana", "mango"
+print(x)
+print(y)
+print(z)
 
 """ this is the multiline comment"""
